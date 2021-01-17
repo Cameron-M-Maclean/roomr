35bf7391a68d70c4b24221911f8ed42a7c6d6f1e
Cameron Maclean <cm.maclean@yahoo.com>
2021-01-17 19:33:40 +0000

    Added to create router and seeds - exported router (can view all in chrome), added to database properties/lesses

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -2,13 +2,40 @@ use roomr_db;
 
 db.dropDatabase();
 
-db.users.insertMany([
-
+db.users.insertMany(
+[
   {
-    firstName: "Cameron",
-    lastName: "Maclean", 
-    studentId: 51337402,
-    university: "University of Edinburgh",
-    yearOfStudy: 3
+    lessees: [
+      {
+        firstName: "Cameron",
+        lastName: "Maclean", 
+        studentId: 51337402,
+        university: "University of Edinburgh",
+        yearOfStudy: 3
+      },
+      {
+        firstName: "Emily",
+        lastName: "Doran", 
+        studentId: 51323402,
+        university: "Napier University",
+        yearOfStudy: 2
+      }
+    ],
+    properties: [
+      {
+        landlord: "Mark Maclean",
+        address: "56/2 Spottiswoode Street",
+        postcode: "EH9 1DH",
+        hmo: true,
+        bedrooms: 4
+      },
+      {
+        landlord: "Anna Maclean",
+        address: "16 Mortonhall Road",
+        postcode: "EH9 2HW",
+        hmo: false,
+        bedrooms: 4 
+      }
+    ]
   }
 ])
