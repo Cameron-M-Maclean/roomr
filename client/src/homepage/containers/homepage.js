864a9476030f82a2031a88c851a44aad93bb95f1
Cameron Maclean <cm.maclean@yahoo.com>
2021-01-18 18:12:11 +0000

    Added homepage css - added positioning for the components within the homepage container

--- client/src/homepage/containers/homepage.js
+++ client/src/homepage/containers/homepage.js
@@ -1,8 +1,12 @@
+//Components 
 import Header from '../components/Header';
 import Body from '../components/Body';
 import StudentSection from '../components/StudentSection';
 import LandlordSection from '../components/LandlordSection';
 import Footer from '../components/Footer';
+//CSS
+import './Homepage.css';
+
 
 function Homepage () {
 
@@ -10,20 +14,22 @@ function Homepage () {
 
   return (
     <>
-      <div>
-        <Header/>
-      </div>
-      <div>
-        <section>
-          <Body/>
-        </section>
-        <section>
-          <StudentSection/>
-          <LandlordSection/>
-        </section>
-      </div>
-      <div>
-        <Footer/>
+      <div id="homepage">
+        <div id="homepage-header">
+          <Header/>
+        </div>
+        <div id="homepage-main-section">
+          <section id="homepage-body">
+            <Body/>
+          </section>
+          <section id="homepage-sections">
+            <StudentSection/>
+            <LandlordSection/>
+          </section>
+        </div>
+        <div id="homepage-footer">
+          <Footer/>
+        </div>
       </div>
     </>
   )
